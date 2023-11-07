d106086b6da9c797fa8ed52df01f754f0a278cc4
keith-kay <keithrhova@gmail.com>
2023-11-07 20:55:18 +0300

    new

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -10,6 +10,7 @@ import os
 from api.v1.auth.auth import Auth
 
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
@@ -20,11 +21,13 @@ if auth_type == 'auth':
 if auth_type == 'basic_auth':
     auth = BasicAuth()
 
+
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     """Unauthorized handler.
     """
     return jsonify({"error": "Unauthorized"}), 401
+
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
@@ -32,11 +35,13 @@ def forbidden(error) -> str:
     """
     return jsonify({"error": "Forbidden"}), 403
 
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
 
 @app.before_request
 def authenticate_user():
@@ -55,6 +60,7 @@ def authenticate_user():
                 abort(401)
             if user is None:
                 abort(403)
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
